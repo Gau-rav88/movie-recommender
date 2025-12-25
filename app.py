7241b358125134ec2349c4e393c82eff75ea04a4
Gaurav Chauhan <chauhangauravgc030@gmail.com>
2025-12-25 17:16:40 +0530

    Fix NameError by removing cosine_sim default

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ st.write("App started successfully")
 
 
 # Function to get movie recommendations
-def get_recommendations(title, cosine_sim=cosine_sim):
+def get_recommendations(title, ):
     idx = movies[movies['title'] == title].index[0]
-    sim_scores = list(enumerate(cosine_sim[idx]))
+    st.error("cosine_sim is not loaded")
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]  # Get top 10 similar movies
     movie_indices = [i[0] for i in sim_scores]
